6697868935773294156ee1dc99fbe14c3ff217f4
me-cinderella <mariana.encalada.ap@gmail.com>
2021-01-15 06:04:44 -0300

    added delete button

--- app/src/components/TutorialsList.js
+++ app/src/components/TutorialsList.js
@@ -1,30 +1,52 @@
 import React, { useState, useEffect } from 'react';
 import { dateFormatterMonthName } from 'utils/Formatters';
-import { Row, Col, Container } from "react-bootstrap";
+import { Button, Row, Col, Container } from "react-bootstrap";
 
-function TutorialsList() {
+function TutorialsList(props) {
 
-    const [error, setError] = useState('');
     const [data, setData] = useState([]);
+    const [submitStatus, setSubmitStatus] = useState('');
 
     useEffect(() => {
-        fetch('https://rayentutorialtestapp.azurewebsites.net/tutorials')
-        .then((response) => response.json())
-        .then((data) => setData(data))
-        .catch(() =>  setError('fetch failed'));
+        setData(props.data);
     }, []) 
+
+
+    const handleDelete = id => event => {
+        event.preventDefault();
+        fetch(`https://rayentutorialtestapp.azurewebsites.net/deletetutorial/${id}`, {
+            method: 'DELETE',
+        })
+        .then((response) => {
+            const newdata = data.filter(item => item.id !== id);
+            setData(newdata);
+            setSubmitStatus('Eliminado exitosamente');
+        })
+        .catch(() => setSubmitStatus('ha ocurrido un error'));
+    }
+
+    console.log(data);
 
     const tutorialsList = data.map( item  => {
         return (
             <Col xs={8} key={item.id} className="border-bottom p-2">
                 <a href={`/tutorial/${item.id}`}>
                     <Row className="tutorial-card align-items-center">
-                        <Col md={6} xs={12}>
+                        <Col md={5} xs={12}>
                             <h5>{item.nombre}</h5>
                             <h6>{item.profesor}</h6>
                         </Col>
-                        <Col md={6} xs={12}>
+                        <Col md={5} xs={12}>
                             <p className="align-top">{dateFormatterMonthName(item.fecha)}</p>
+                        </Col>
+                        <Col md={2} xs={12}>
+                            <Button 
+                                className="btn btn-default" 
+                                aria-label="Left Align"
+                                onClick={handleDelete(item.id)}
+                            >
+                                eliminar
+                            </Button>
                         </Col>
                     </Row>
                 </a>
@@ -38,7 +60,7 @@ function TutorialsList() {
             <h1>Tutoriales</h1>
         </Row>
         <Row className="align-center">
-            { tutorialsList ? tutorialsList : <p>{error}</p> }
+            { tutorialsList ? tutorialsList : <p>{props.error}</p> }
         </Row>
         </Container>
     );
--- app/src/views/Home.js
+++ app/src/views/Home.js
@@ -1,20 +1,53 @@
-import React from 'react';
-import { Col, Container, Row } from "react-bootstrap";
+import React, { useState, useEffect } from 'react';
+import { Link } from "react-router-dom";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import TutorialsList from 'components/TutorialsList';
-import AddButton from 'components/AddButton';
 
 function Home() {
+
+    const [error, setError] = useState('');
+    const [data, setData] = useState([]);
+    const [submitStatus, setSubmitStatus] = useState('');
+
+    useEffect(() => {
+        fetch('https://rayentutorialtestapp.azurewebsites.net/tutorials')
+        .then((response) => response.json())
+        .then((data) => setData(data))
+        .catch(() =>  setError('fetch failed'));
+    }, []) 
+
+    const handleDelete = () => {
+        fetch('https://rayentutorialtestapp.azurewebsites.net/deletetutorials', {
+            method: 'DELETE',
+        })
+        .then((response) => {
+            setData({});
+            setSubmitStatus('Eliminado exitosamente');
+        })
+        .catch(() => setSubmitStatus('ha ocurrido un error'));
+    }
     
     return (
         <Container>
             <Row className="justify-content-md-center">
                 <Col xs={8}>
-                    <TutorialsList />
+                    <TutorialsList data={[{"nombre":"Curso 1", "materia": "Materia 1", "profesor": "profesor1", "fecha": new Date()}]} error={error} />
                 </Col>
             </Row>
             <Row className="justify-content-md-center m-5">
-                <Col offset={8} xs={2}>
-                    <AddButton />
+                <Col offset={6} xs={6}>
+                    <Link to='/agregar'>
+                        <Button type="button" className="btn btn-default" aria-label="Left Align">
+                            +
+                        </Button>
+                    </Link>
+                    <Button 
+                        className="btn btn-default" 
+                        aria-label="Left Align"
+                        onClick={() => handleDelete()}
+                    >
+                        Borrar todos
+                    </Button>
                 </Col>
             </Row>
         </Container>
